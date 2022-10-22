e44860d77b0ace2a4431ba9a9c1f965b6e649e0a
Daniel <15sanchd@gmail.com>
2022-10-22 14:48:26 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-import React, { useState } from 'react';
+import React, { useState, ErrorBoundary } from 'react';
 import {
   RecoilRoot,
   atom,
   selector,
+  selectorFamily,
   useRecoilState,
   useRecoilValue,
   useSetRecoilState
@@ -47,6 +48,29 @@ function TodoItemCreator(){
   );
 
 }
+
+const todoListFilterState = atom({
+  key: 'TodoListFilter',
+  default: 'Show All',
+});
+
+
+const filteredTodoListState = selector({
+  key: 'FilteredTodoList',
+  get: ({get}) => {
+    const filter = get(todoListFilterState);
+    const list = get(todoListState);
+
+    switch (filter) {
+      case 'Show Completed':
+        return list.filter((item) => item.isComplete);
+      case 'Show Uncompleted':
+        return list.filter((item) => !item.isComplete);
+      default:
+        return list;
+    }
+  },
+});
 
 
 // utility for creating unique Id
@@ -97,6 +121,66 @@ function TodoItem({item}) {
   );
 }
 
+function TodoListFilter(){
+
+  const [filter, setFilter] = useRecoilState(todoListFilterState)
+
+
+  const updateFilter = (e) => {
+    setFilter(e.value);
+  };
+
+
+  return (
+    <>
+      Filter:
+      <select value={filter} onChange={updateFilter}>
+        <option value="Show All">All</option>
+        <option value="Show Completed">Completed</option>
+        <option value="Show Uncompleted">Uncompleted</option>
+      </select>
+    </>
+  )
+}
+
+const todoListStatsState = selector({
+  key: 'TodoListStats',
+  get: ({get}) => {
+    const todoList = get(todoListState);
+    const totalNum = todoList.length;
+    const totalCompletedNum = todoList.filter((item) => item.isComplete).length;
+    const totalUncompletedNum = totalNum - totalCompletedNum;
+    const percentCompleted = totalNum === 0 ? 0 : totalCompletedNum / totalNum * 100;
+
+    return {
+      totalNum,
+      totalCompletedNum,
+      totalUncompletedNum,
+      percentCompleted,
+    };
+  },
+});
+
+function TodoListStats() {
+  const {
+    totalNum,
+    totalCompletedNum,
+    totalUncompletedNum,
+    percentCompleted,
+  } = useRecoilValue(todoListStatsState);
+
+  const formattedPercentCompleted = Math.round(percentCompleted);
+
+  return (
+    <ul>
+      <li>Total items: {totalNum}</li>
+      <li>Items completed: {totalCompletedNum}</li>
+      <li>Items not completed: {totalUncompletedNum}</li>
+      <li>Percent completed: {formattedPercentCompleted}</li>
+    </ul>
+  );
+}
+
 function replaceItemAtIndex(arr, index, newValue) {
   return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)];
 }
@@ -112,7 +196,10 @@ function TodoList(){
     <>
       {/* todo */}
       {/* todo */}
-        
+
+      <TodoListStats />
+      <TodoListFilter />    
+
       <TodoItemCreator />      
       { todoList.map((todoItem) => (
           <TodoItem key={todoItem.id} item={todoItem}/>
@@ -121,6 +208,7 @@ function TodoList(){
     </>
   )
 }
+
 
 
 function App() {
